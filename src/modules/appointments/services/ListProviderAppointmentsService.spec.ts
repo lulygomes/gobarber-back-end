748ba6bf90b3d1d2f8d27869db6b5118675c97ca
Luiz Antonio Gomes da Silva <luiz.gsilva42@gmail.com>
2020-10-02 11:24:46 -0300

    Agenda do prestador

--- src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -14,15 +14,15 @@ describe('ListProviderAppointments', () => {
     );
   });
 
-  it('should be able to list the day availability from provider', async () => {
-    await fakeAppointmentsRepository.create({
-      provider_id: 'user',
+  it('should be able to list the appointments on a specific day', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
       user_id: '123456',
       date: new Date(2020, 4, 20, 14, 0, 0),
     });
 
-    await fakeAppointmentsRepository.create({
-      provider_id: 'user',
+    const appointment2 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
       user_id: '123456',
       date: new Date(2020, 4, 20, 15, 0, 0),
     });
@@ -31,23 +31,13 @@ describe('ListProviderAppointments', () => {
       return new Date(2020, 4, 20, 11).getTime();
     });
 
-    const availability = await listProviderDayAvailability.execute({
-      provider_id: 'user',
+    const appointments = await listProviderAppointments.execute({
+      provider_id: 'provider',
       day: 20,
       year: 2020,
       month: 5,
     });
 
-    expect(availability).toEqual(
-      expect.arrayContaining([
-        { hour: 8, available: false },
-        { hour: 9, available: false },
-        { hour: 10, available: false },
-        { hour: 13, available: true },
-        { hour: 14, available: false },
-        { hour: 15, available: false },
-        { hour: 16, available: true },
-      ]),
-    );
+    expect(appointments).toEqual([appointment1, appointment2]);
   });
 });
