9f76a43bfbc812ab4a3043a3169cafdfff211027
Luiz Antonio Gomes da Silva <luiz.gsilva42@gmail.com>
2020-10-16 15:39:49 -0300

    Configuração redis

--- src/shared/container/provider/CacheProvider/implementations/RedisCacheProvider.ts
+++ src/shared/container/provider/CacheProvider/implementations/RedisCacheProvider.ts
@@ -1,11 +1,12 @@
 import Redis, { Redis as RedisClient } from 'ioredis';
+import cacheConfig from '@config/cache';
 import ICacheProvider from '../models/ICacheProvider';
 
 export default class RedisCacheProvider implements ICacheProvider {
   private client: RedisClient;
 
   constructor() {
-    this.client = new Redis();
+    this.client = new Redis(cacheConfig.config.redis);
   }
 
   save(key: string, value: string): Promise<void> {}
