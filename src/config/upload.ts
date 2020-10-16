0975d697f79106cbc7aed4cfdcd940fbee6963f7
Luiz Antonio Gomes da Silva <luiz.gsilva42@gmail.com>
2020-10-16 11:11:05 -0300

    upload para amazon s3

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,20 +1,48 @@
 import crypto from 'crypto';
 import path from 'path';
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+
+  tmpFolder: string;
+  uploadFolder: string;
+
+  multer: {
+    storage: StorageEngine;
+  };
+
+  config: {
+    disk: {};
+    aws: {
+      bucket: string;
+    };
+  };
+}
+
 export default {
+  driver: process.env.STORAGE_DRIVER,
   tmpFolder,
   uploadFolder: path.resolve(tmpFolder, 'uploads'),
 
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('hex');
-      const fileName = `${fileHash}-${file.originalname}`;
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('hex');
+        const fileName = `${fileHash}-${file.originalname}`;
 
-      return callback(null, fileName);
+        return callback(null, fileName);
+      },
+    }),
+  },
+
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'app-gobarber-profile-img',
     },
-  }),
-};
+  },
+} as IUploadConfig;
